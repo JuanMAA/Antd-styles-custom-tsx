0ac25e6255f3eff0816bbf54eb2a3f316bcdc0c4
Juan Mansilla Asenjo <juan.mansilla@Egt.cl>
2021-02-22 12:22:52 -0300

    Mas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { cambiarColor } from './functions.js'
+import { Button, Switch, Space, Menu } from "antd"; 
+import styles from './App.module.less';
+const { SubMenu } = Menu;
 
-function App() {
+
+const App = () => {
+
+  const handleSwitchChange = (checked: any) => {
+    cambiarColor(checked)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.app}>
+      <Menu mode="horizontal">
+        <Menu.Item key="mail" >
+          Navigation One
+        </Menu.Item>
+        <Menu.Item key="app" disabled >
+          Navigation Two
+        </Menu.Item>
+        <SubMenu key="SubMenu" title="Navigation Three - Submenu">
+          <Menu.ItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+        <Menu.Item key="alipay">
+          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+            Navigation Four - Link
+          </a>
+        </Menu.Item>
+      </Menu>
+      <div style={{ textAlign: "center" }}>
+        <Space>
+          Change Color
+            <Switch onChange={handleSwitchChange} />
+        </Space>
+      </div>
+      <Button type="primary">Hello, Ant Design!</Button>
+      <a href="foo.bar">I'm a link. Click me, please!</a>
+    </main>
   );
-}
+};
 
 export default App;
